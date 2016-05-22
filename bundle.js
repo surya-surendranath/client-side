23a30485d4ba3e1bc0d9f6634d3c626b5cdb6d22
surya-surendranath & Uili Fecteau <pair+surya-surendranath.uili-Simteau@devbootcamp.com>
2016-05-23 11:50:09 +1200

    Updates

--- bundle.js
+++ bundle.js
@@ -89,6 +89,6 @@ function once(n){var o=!1;return function(){return o?void 0:(o=!0,n.apply(this,a
 function extend(){for(var r={},e=0;e<arguments.length;e++){var t=arguments[e];for(var n in t)hasOwnProperty.call(t,n)&&(r[n]=t[n])}return r}module.exports=extend;var hasOwnProperty=Object.prototype.hasOwnProperty;
 
 },{}],30:[function(require,module,exports){
-var HandlebarsCompiler=require("hbsfy/runtime");module.exports=HandlebarsCompiler.template({compiler:[7,">= 4.0.0"],main:function(n,e,a,l,r){var t,o=null!=e?e:{},u=a.helperMissing,p="function",i=n.escapeExpression;return"h1>Hello "+i((t=null!=(t=a.name||(null!=e?e.name:e))?t:u,typeof t===p?t.call(o,{name:"name",hash:{},data:r}):t))+'!</h1>\n\n<p id="paragraph">'+i((t=null!=(t=a.body||(null!=e?e.body:e))?t:u,typeof t===p?t.call(o,{name:"body",hash:{},data:r}):t))+'</p>\n\n<button id="button">Where???</button>\n\n'},useData:!0});
+var HandlebarsCompiler=require("hbsfy/runtime");module.exports=HandlebarsCompiler.template({compiler:[7,">= 4.0.0"],main:function(n,e,a,l,r){var t,o=null!=e?e:{},u=a.helperMissing,p="function",i=n.escapeExpression;return"<h1>Hello "+i((t=null!=(t=a.name||(null!=e?e.name:e))?t:u,typeof t===p?t.call(o,{name:"name",hash:{},data:r}):t))+'!</h1>\n\n<p id="paragraph">'+i((t=null!=(t=a.body||(null!=e?e.body:e))?t:u,typeof t===p?t.call(o,{name:"body",hash:{},data:r}):t))+'</p>\n\n<button id="button">Where???</button>\n\n'},useData:!0});
 
 },{"hbsfy/runtime":23}]},{},[1]);
