9204f6048c26bea8c75868a5d9bcc305b0c08070
surya-surendranath & Uili Fecteau <pair+surya-surendranath.uili-Simteau@devbootcamp.com>
2016-05-23 11:04:08 +1200

    Release 1 completed

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+var satellite = JSON.parse(data.body.replace('/[|]/g',''))
+console.log(satellite[0].name)
+  document.body.innerHTML = example({ name: satellite[0].name });
 })
 
