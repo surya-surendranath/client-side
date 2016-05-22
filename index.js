22a27fa1e911a9771c415475503b156cf4b2bc46
surya-surendranath & Uili Fecteau <pair+surya-surendranath.uili-Simteau@devbootcamp.com>
2016-05-23 10:32:59 +1200

    Updates

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+  document.body.innerHTML = example({ name: "Space station" });
 })
 
