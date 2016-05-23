61457bee0faadeb91fd68b812eb85c1184d5fab5
surya-surendranath & Uili Fecteau <pair+surya-surendranath.uili-Simteau@devbootcamp.com>
2016-05-23 13:48:08 +1200

    Updates

--- index.js
+++ index.js
@@ -3,22 +3,20 @@ var example = require('./views/example.hbs')
 var whereisiss = require('./views/whereisiss.hbs')
 
 xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-  if (err) console.log(err) // do something
+  if (err) console.log(err)
 
-    console.log(data.body)
-  var satellite = JSON.parse(data.body.replace('/[|]/g',''))
-  console.log(satellite[0].name)
-  document.body.innerHTML = example({ name: satellite[0].name });
-  console.log(data)
+    var satellite = JSON.parse(data.body.replace('/[|]/g',''))
 
+    console.log(satellite[0].name)
+    document.body.innerHTML = example({ name: satellite[0].name });
+    console.log(data)
 
-
-  document.getElementById('button').addEventListener('click',function(){
-    xhr.get('https://api.wheretheiss.at/v1/satellites/' + satellite[0].id, function(err, data) {
-  if (err) console.log(err) // do something
-    console.log("hello", (err) ? err:data)
-  var iss = JSON.parse(data.body)
-  document.getElementById('paragraph').innerHTML =whereisiss(iss)
+    document.getElementById('button').addEventListener('click',function(){
+      xhr.get('https://api.wheretheiss.at/v1/satellites/' + satellite[0].id, function(err, data) {
+          if (err) console.log(err) // do something
+          console.log("hello", (err) ? err:data)
+          var iss = JSON.parse(data.body)
+          document.getElementById('paragraph').innerHTML =whereisiss(iss)
 })
   })
 
