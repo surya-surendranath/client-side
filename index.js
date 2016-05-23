a73723693fb8b41c00aef788d915e6015fefc98c
surya-surendranath & Uili Fecteau <pair+surya-surendranath.uili-Simteau@devbootcamp.com>
2016-05-23 12:04:15 +1200

    Release 3 working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
+var whereisiss = require('./views/whereisiss.hbs')
 
 xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
@@ -8,5 +9,17 @@ xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
 var satellite = JSON.parse(data.body.replace('/[|]/g',''))
 console.log(satellite[0].name)
   document.body.innerHTML = example({ name: satellite[0].name });
+  console.log(data)
+
+
+
+document.getElementById('button').addEventListener('click',function(){
+  xhr.get('https://api.wheretheiss.at/v1/satellites/' + satellite[0].id, function(err, res) {
+  if (err) console.log(err) // do something
+console.log("hello", (err) ? err:res)
+var iss = JSON.parse(res.body)
+document.getElementById('paragraph').innerHTML =whereisiss(iss)
+})
 })
 
+})
